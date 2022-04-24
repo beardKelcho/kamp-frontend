0efdebac21c3cb07e69c27eb1321fd4a313e086a
kaan colak <c.kaaan@gmail.com>
2022-04-24 22:17:03 +0300

    24.04 commit

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/ListResponseModel';
+import { Product } from '../models/product';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiUrl = "https://localhost:44350/api/products/getall";
+  constructor(private httpClient: HttpClient) { }
+
+  getPorducts():Observable<ListResponseModel<Product>> {
+    return this.httpClient
+      .get<ListResponseModel<Product>>(this.apiUrl);
+      
+  }
+}
