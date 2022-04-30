48c4a0103eaf47b988e94b9177927b381ef1a67c
kaan colak <c.kaaan@gmail.com>
2022-04-30 13:54:09 +0300

    21 ders sonu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
-import { FormsModule } from "@angular/forms";
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
+import { FormsModule,ReactiveFormsModule } from "@angular/forms";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +14,10 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 import { Toast, ToastrModule } from 'ngx-toastr';
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import {ProductAddComponent} from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+
 
 
 
@@ -25,7 +29,9 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     NaviComponent,
     VatAddedPipe,
     FilterPipePipe,
-    CartSummaryComponent
+    CartSummaryComponent,
+    ProductAddComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,9 +39,12 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     ToastrModule.forRoot({positionClass:'toast-bottom-right'})
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS, useClass:AuthInterceptor, multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
